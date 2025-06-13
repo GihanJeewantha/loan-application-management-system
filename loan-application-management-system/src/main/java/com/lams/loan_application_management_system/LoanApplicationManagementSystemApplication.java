bd3b7fad13289392faab1b51c8033cfde291431e
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2025-06-13 09:35:35 +0530

    Getting Started with LAMS: Setup and Project

---
+++ loan-application-management-system/src/main/java/com/lams/loan_application_management_system/LoanApplicationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lams.loan_application_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplicationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ loan-application-management-system/src/test/java/com/lams/loan_application_management_system/LoanApplicationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lams.loan_application_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
